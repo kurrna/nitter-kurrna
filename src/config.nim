0b929ca6106c29e860382ca02be6efb8ff594744
Kurna <kurna@foxmail.com>
2026-01-18 22:31:49 +0800

    fix the host in config

--- src/config.nim
+++ src/config.nim
@@ -21,7 +21,7 @@ proc getConfig*(path: string): (Config, parseCfg.Config) =
     httpMaxConns: cfg.get("Server", "httpMaxConnections", 100),
     staticDir: cfg.get("Server", "staticDir", "./public"),
     title: cfg.get("Server", "title", "Nitter"),
-    hostname: cfg.get("Server", "hostname", "nitter-kurrna.net"),
+    hostname: cfg.get("Server", "hostname", "nitter-kurrna.fly.dev"),
 
     # Cache
     listCacheTime: cfg.get("Cache", "listMinutes", 120),
